6bf1cd1a755f89964b5ef8be5302f76d2e03fd82
Gazi2050 <gazi45344@gmail.com>
2023-12-10 20:19:06 +0600

    Header added

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,9 +1,65 @@
-import { Box } from "@mui/material";
+"use client"
+import { Box, Button, Container, IconButton, Typography } from "@mui/material";
+//icons
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import RedditIcon from '@mui/icons-material/Reddit';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Link from "next/link";
 
 const Footer = () => {
+    const navItems = [
+        {
+            route: "Home",
+            pathname: "/"
+        },
+        {
+            route: "Pages",
+            pathname: "/pages"
+        },
+        {
+            route: "Category",
+            pathname: "/category"
+        },
+        {
+            route: "News",
+            pathname: "/news"
+        },
+        {
+            route: "About",
+            pathname: "/about"
+        },
+        {
+            route: "Contact",
+            pathname: "/contact"
+        },
+    ];
     return (
-        <Box>
-
+        <Box className="bg-black px-2 py-10">
+            <Container>
+                <Box className="text-center">
+                    <IconButton>
+                        <FacebookRoundedIcon className='text-blue-500' />
+                    </IconButton>
+                    <IconButton>
+                        <RedditIcon className='text-orange-500' />
+                    </IconButton>
+                    <IconButton>
+                        <InstagramIcon className='text-red-400' />
+                    </IconButton>
+                </Box>
+                <Box className="w-full text-center">
+                    {navItems.map((item) => (
+                        <Link
+                            key={item}
+                            href={item.pathname}>
+                            <Button className="text-white">
+                                {item.route}
+                            </Button>
+                        </Link>
+                    ))}
+                </Box>
+                <Typography className="text-slate-500 text-center">© 2023 Dragon News. All Rights Reserved</Typography>
+            </Container>
         </Box>
     );
 };
---
+++ src/components/shared/Header.js
@@ -0,0 +1,14 @@
+import { Box, Container } from '@mui/material';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <Box>
+            <Container>
+
+            </Container>
+        </Box>
+    );
+};
+
+export default Header;
